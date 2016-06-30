53001efa8f17d851b9d78a96fbc1146a66851ca8
Zeno Rocha <hi@zenorocha.com>
2016-06-30 14:15:49 -0700

    Replaces double quotes in favor of single quotes

--- scripts/index.js
+++ scripts/index.js
@@ -3,21 +3,21 @@
 var slider = document.getElementById('song-progress');
 
 noUiSlider.create(slider, {
-	start: [ 20 ],
-	range: {
-		'min': [ 0 ],
-		'max': [ 100 ]
-	}
+  start: [ 20 ],
+  range: {
+    min: [ 0 ],
+    max: [ 100 ]
+  }
 });
 
 var slider = document.getElementById('song-volume');
 
 noUiSlider.create(slider, {
-	start: [ 90 ],
-	range: {
-		'min': [ 0 ],
-		'max': [ 100 ]
-	}
+  start: [ 90 ],
+  range: {
+    min: [ 0 ],
+    max: [ 100 ]
+  }
 });
 
 // Tooltips
@@ -28,7 +28,7 @@ $(function () {
 
 // Viewport Heights
 
-$(window).on("resize load", function(){
+$(window).on('resize load', function(){
   var totalHeight = $(window).height();
 
   var headerHeight = $('.header').outerHeight();
@@ -39,71 +39,71 @@ $(window).on("resize load", function(){
   var navHeight = totalHeight - (headerHeight + footerHeight + playlistHeight + nowPlaying);
   var artistHeight = totalHeight - (headerHeight + footerHeight);
 
-  $(".navigation").css("height" , navHeight);
-  $(".artist").css("height" , artistHeight);
-  $(".social").css("height" , artistHeight);
+  $('.navigation').css('height', navHeight);
+  $('.artist').css('height', artistHeight);
+  $('.social').css('height', artistHeight);
 });
 
 // Collapse Toggles
 
-$(".navigation__list__header").on( "click" , function() {
-  $(this).toggleClass( "active" );
+$('.navigation__list__header').on( 'click' , function() {
+  $(this).toggleClass( 'active' );
 });
 
 
 // Media Queries
 
-$(window).on("resize load", function(){
-	if ($(window).width() <= 768) {
-    $(".collapse").removeClass("in");
-    $(".navigation").css("height" , "auto");
-    $(".artist").css("height" , "auto");
-	}
+$(window).on('resize load', function(){
+  if ($(window).width() <= 768) {
+    $('.collapse').removeClass('in');
+    $('.navigation').css('height' , 'auto');
+    $('.artist').css('height' , 'auto');
+  }
 });
 
-$(window).on("resize load", function(){
-	if ($(window).width() > 768){
-    $(".collapse").addClass("in");
-	}
+$(window).on('resize load', function(){
+  if ($(window).width() > 768){
+    $('.collapse').addClass('in');
+  }
 });
 
 // Tracks
 
 var tracks = [
   {
-    artist: "Awolnation",
-    song: "Sail",
-    url: "musics/sail.mp3"
+    artist: 'Awolnation',
+    song: 'Sail',
+    url: 'musics/sail.mp3'
   },
   {
-    artist: "Bastille",
-    song: "Pompeii",
-    url: "musics/pompeii.mp3"
+    artist: 'Bastille',
+    song: 'Pompeii',
+    url: 'musics/pompeii.mp3'
   },
   {
-    artist: "Deadmau5",
-    song: "Ghosts n' Stuff",
-    url: "musics/ghosts_n_stuff.mp3"
+    artist: 'Deadmau5',
+    song: 'Ghosts n\' Stuff',
+    url: 'musics/ghosts_n_stuff.mp3'
   },
   {
-    artist: "Foster The People",
-    song: "Pumped Up Kicks",
-    url: "musics/pumped_up_kicks.mp3"
+    artist: 'Foster The People',
+    song: 'Pumped Up Kicks',
+    url: 'musics/pumped_up_kicks.mp3'
   },
   {
-    artist: "Ghost Town",
-    song: "You're So Creepy",
-    url: "musics/youre_so_creepy.mp3"
+    artist: 'Ghost Town',
+    song: 'You\'re So Creepy',
+    url: 'musics/youre_so_creepy.mp3'
   },
   {
-    artist: "Klaypex",
-    song: "Gamefire",
-    url: "musics/gamefire.mp3"
+    artist: 'Klaypex',
+    song: 'Gamefire',
+    url: 'musics/gamefire.mp3'
   },
   {
-    artist: "The Glitch Mob",
-    song: "Seven Nation Army",
-    url: "musics/seven_nation_army.mp3"
+    artist: 'The Glitch Mob',
+    song: 'Seven Nation Army',
+    url: 'musics/seven_nation_army.mp3'
   }
 ];
 
@@ -195,13 +195,13 @@ setInterval(function() {
 // Format Time
 
 function formatTime(seconds) {
-    seconds = Math.floor(seconds);
+  seconds = Math.floor(seconds);
 
-    minutes = Math.floor(seconds / 60);
-    minutes = (minutes >= 10) ? minutes : "0" + minutes;
+  minutes = Math.floor(seconds / 60);
+  minutes = (minutes >= 10) ? minutes : '0' + minutes;
 
-    seconds = Math.floor(seconds % 60);
-    seconds = (seconds >= 10) ? seconds : "0" + seconds;
+  seconds = Math.floor(seconds % 60);
+  seconds = (seconds >= 10) ? seconds : '0' + seconds;
 
-    return minutes + ":" + seconds;
+  return minutes + ':' + seconds;
 }
